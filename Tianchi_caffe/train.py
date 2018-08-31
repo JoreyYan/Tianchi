b0e31057d3910f72f30a1ef3feba56def989558f
JoreyYan <35975693+JoreyYan@users.noreply.github.com>
2018-08-31 23:13:23 +0800

    Update train.py

--- Tianchi_caffe/train.py
+++ Tianchi_caffe/train.py
@@ -5,7 +5,7 @@ import os
 import sys
 from config import opt
 if opt.online:
-    os.environ["PYTHONPATH"]="%s:%s"%("/home/caffe/python","/workspace/pai")
+
     sys.path.append("/home/caffe/python")
     sys.path.append("/workspace/pai")
 #import ipdb;ipdb.set_trace()
@@ -18,42 +18,26 @@ import psutil
 from utils.confuse import ConfusionMeter
 #from utils import sysinfo
 import subprocess
-logging.basicConfig(level=logging.DEBUG,
-                format='%(asctime)s %(message)s',
-                datefmt='%a, %d %b %Y %H:%M:%S',
-                filename='myapp.log', #修改log地址为跳板机上的文件
-                filemode='w')
-
 
 
 seg_model_path=None#opt.model_seg_pre_weight
 
-# 数据读取4核 dataloader.num_workers=4，下标：64-67
-# 运行程序是60核，下标：4-63
-# PAI平台占用 下标：0-3
-if opt.online==False:
-    os.environ["OMP_NUM_THREADS"]='55'
-    os.environ["KMP_AFFINITY"]="explicit,1,0,granularity=fine,proclist=[4-58]"
-else:
-    os.environ["OMP_NUM_THREADS"]='60'
-    os.environ["KMP_AFFINITY"]="explicit,1,0,granularity=fine,proclist=[4-63]"
+
 def train_seg():
     #subprocess.Popen('top')
     solver = caffe.AdamSolver('solver/seg_solver.prototxt')
-    if seg_model_path is not None:
-        print "loading pre-model.",seg_model_path
-        solver.net.copy_from(seg_model_path)
-
+   
     # 等价于solver文件中的max_iter，即最大解算次数  
     niter = 10000  
     # 每隔100次收集一次数据  
-    display= 50  if opt.online==False else 20
+    display= 50  
+    
     display_=622
     
 
     #初始化 
-    train_loss = np.zeros(niter / display)
-    train_loss_ = np.zeros(niter / display_)
+    train_loss = np.zeros(niter / display) #是每50次的均值loss
+    train_loss_ = np.zeros(niter / display_)#是每622次的均值loss
     
     solver.step(1)  
 
@@ -63,42 +47,38 @@ def train_seg():
     # 进行解算  
     #loss_meter = tnt.meter.AverageValueMeter()
     #loss_meter.reset()
+    #10000次以循环的方式 一步步进行
     for it in range(niter):  
         #import ipdb;ipdb.set_trace()
-        # 进行一次解算  
+        # 进行一次解算  并获得loss数据
         solver.step(1) 
         now_loss=solver.net.blobs["loss"].data 
         #loss_meter.add(now_loss[0])
-        # 每迭代一次，训练batch_size张图片  
+        # 每迭代一次，训练batch_size张图片 
+        #对loss进行累加 _前缀的都是累加值
         _train_loss += now_loss
         _all_loss +=now_loss
         #print "setp: ",it,"time: ",time.strftime('%m-%d %H:%M:%S') ,"  train_loss average:",_all_loss/(it%display_+1) 
         if os.path.exists("/tmp/debug"):
             import ipdb; ipdb.set_trace()
+        
+        #训练每622次之后输出一次平均损失 all loss进行归零  train_loss_all_average 是每622次输出的平均损失    
         if it % display_ == 0: 
             train_loss_[it // display_] = _all_loss / display_         
             print "step 622: %s train_loss average:"%(time.strftime('%m-%d %H:%M:%S')) , train_loss_[it // display_]
             _all_loss=0
             #loss_meter.reset()
+        
+        #训练每50次之后输出一次平均损失 train loss进行归零  train_loss 是每次50输出的平均损失 
         if it % display == 0:  
             # 计算平均train loss  
             train_loss[it // display] = _train_loss / display
             print psutil.cpu_percent(percpu=True), psutil.virtual_memory()           
             print "step 50: %s train_loss average:"%(time.strftime('%m-%d %H:%M:%S')) , train_loss[it // display]
             _train_loss = 0  
-            #vis_plots = {'loss':loss_meter.value()[0],'it':it}
-            #vis.plot_many(vis_plots)
-
-            #iik=np.random.randint(0,4,1)[0]
-            #image=t.from_numpy(solver.net.blobs["data"].data[iik] )
-            #probs=t.from_numpy(solver.net.blobs["probs"].data[iik])
-            #mask=t.from_numpy(solver.net.blobs["label"].data[iik] )
-            #print image.size(),probs.size(), mask.size()
-            #vis.vis.histogram(
-             #      probs.view(-1), win=u'output_hist', opts=dict(title='output_hist'))
-                #！TODO: tell 代成 make 1/3 和1 ，而不是1和3
-            #vis_imgs = {'input':image,'mask':mask,'output':probs}
-            #vis.img_grid_many(vis_imgs)
+            
+            
+    #保存每50次的均值loss        
     np.save("train_loss.npy",train_loss)  
     del solver  
 
